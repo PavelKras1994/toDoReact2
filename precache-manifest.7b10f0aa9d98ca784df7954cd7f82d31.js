94bbde54b3d5dd69c0fab99befb3c04c4e8389da
Pavel <pavekrasnoschok@gmai.com>
2020-08-12 18:17:16 +0300

    new ToDo build with style

---
+++ precache-manifest.7b10f0aa9d98ca784df7954cd7f82d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2488d094da36a2527f6161e365e9f854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd3c02a9a7bdd608079",
+    "url": "/static/css/main.fa06426a.chunk.css"
+  },
+  {
+    "revision": "dd287c8e788a3bde5076",
+    "url": "/static/js/2.d20ce4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d20ce4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd3c02a9a7bdd608079",
+    "url": "/static/js/main.288ac10f.chunk.js"
+  },
+  {
+    "revision": "eabcc1cc6f081c417319",
+    "url": "/static/js/runtime-main.5c22a0b3.js"
+  }
+]);
